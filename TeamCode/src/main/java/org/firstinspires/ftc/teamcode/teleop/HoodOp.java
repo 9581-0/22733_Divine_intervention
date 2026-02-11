d1ee9a7eafa12a3148b868ae4826643ab6c58d9f
9581-0 <tmaekawa2027@ravenscroft.org>
2026-02-10 23:50:31 -0500

    sloop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HoodOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HoodOp.java
@@ -15,18 +15,16 @@ public class HoodOp extends LinearOpMode {
         // The string "hood" must match the name in your Robot Configuration.
         hood = hardwareMap.get(Servo.class, "hood");
 
-        telemetry.addData("Status", "Initialized. Press Play to zero hood.");
-        telemetry.update();
-
         waitForStart();
 
         while (opModeIsActive()) {
             // Send the command to move to position 0
-            if (gamepad1.a) {hood.setPosition(0.0);}
 
             telemetry.addData("Servo Position", hood.getPosition());
             telemetry.addData("Status", "Running - Forced to 0");
             telemetry.update();
+            if (gamepad1.a) {hood.setPosition(0.0);}
+
         }
     }
 }
