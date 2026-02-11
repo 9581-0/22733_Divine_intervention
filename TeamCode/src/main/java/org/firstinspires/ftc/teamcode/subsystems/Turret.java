970a27a8eec58e56322c443d4ae706bc0de610a9
9581-0 <tmaekawa2027@ravenscroft.org>
2026-02-10 21:15:09 -0500

    slopturret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -2,26 +2,27 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.util.Pose2d;
-import org.firstinspires.ftc.teamcode.util.PIDF;
-import org.firstinspires.ftc.teamcode.util.drivers.GoBildaPinpointDriver;
 
 public class Turret {
-    public static final double GEAR_RATIO = (double) 80 /20* (double) 37/112;
+    // Calculated Gear Ratio (~1.321).
+    // If turret moves TOO FAR, change this to (1.0 / GEAR_RATIO)
+    public static final double GEAR_RATIO = (80.0 / 20.0) * (37.0 / 112.0);
+
+    // Total physical range of your servo in radians (Defaulting to 300 degrees)
+    public static final double SERVO_RANGE_RAD = Math.toRadians(300);
+
     public static double GOAL_X = 0.0, GOAL_Y = 0.0;
 
     private final Servo LServo, RServo;
-
-    public double target;
+    public double target = 0.5;
     public static boolean tracking = false;
 
     public Turret (HardwareMap map) {
         LServo = map.get(Servo.class, "TurretServoL");
         RServo = map.get(Servo.class, "TurretServoR");
+        setPos(0.5); // Initialize to center
     }
-
 
     public void update(Pose2d currentPosition) {
         if (!tracking) return;
@@ -29,47 +30,39 @@ public class Turret {
         double dx = GOAL_X - currentPosition.getX();
         double dy = GOAL_Y - currentPosition.getY();
 
+        // Calculate angle from robot to target
         double angleToGoal = Math.atan2(dy, dx);
+
+        // Error is the difference between goal and current robot heading
+        // We subtract Math.PI if the turret "zero" is facing the back of the robot
         double error = angleWrap(angleToGoal - currentPosition.getHeading() - Math.PI);
 
-        double servoDelta = (error  / GEAR_RATIO);
+        // Map the radian error to a 0.0 - 1.0 servo value using gear ratio
+        double servoDelta = (error * GEAR_RATIO) / SERVO_RANGE_RAD;
 
+        // Apply delta to the center position (0.5)
         double pos = clamp(0.5 + servoDelta);
         setPos(pos);
     }
 
-    public double clamp(double v) {
-        return Math.max(0.0, Math.min(1.0, v));
-    }
-
-
-    private double angleWrap(double a) {
-        while (a > Math.PI) a -= 2 * Math.PI;
-        while (a < -Math.PI) a += 2 * Math.PI;
-        return a;
-    }
-
-    public void sotm() {
-
-     }
-
-     public void setGoalPositions(double x, double y){
-         GOAL_X = x;
-         GOAL_Y = y;
-     }
-
     private void setPos(double pos){
-        LServo.setPosition(pos);
-        RServo.setPosition(pos);
         target = pos;
+        LServo.setPosition(pos);
+        // Mirroring RServo: if one goes 0.7, the other goes 0.3 to rotate the same direction
+        RServo.setPosition(1.0 - pos);
+    }
+
+    public void setGoalPositions(double x, double y){
+        GOAL_X = x;
+        GOAL_Y = y;
     }
 
     public double getPosition(){
         return LServo.getPosition();
     }
 
-
     public void setPosition(double pos){
+        tracking = false; // Disable auto-tracking when manual position is set
         setPos(pos);
     }
 
@@ -80,5 +73,15 @@ public class Turret {
     public void setHalf() {
         tracking = false;
         setPos(0.5);
+    }
+
+    private double angleWrap(double a) {
+        while (a > Math.PI) a -= 2 * Math.PI;
+        while (a < -Math.PI) a += 2 * Math.PI;
+        return a;
+    }
+
+    private double clamp(double v) {
+        return Math.max(0.0, Math.min(1.0, v));
     }
 }
